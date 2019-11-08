8d2c37ca644d807bf525af30eb3427dede1747d3
Rok Garbas <rok@garbas.si>
2019-11-08 17:04:16 +0100

    linting

--- api/src/tooltool_api/lib/pulse.py
+++ api/src/tooltool_api/lib/pulse.py
@@ -95,7 +95,7 @@ async def _create_consumer(user, password, exchange, topic, callback):
         try:
             await protocol.ensure_open()
         # raise AmqpClosedConnection in case the connection is closed.
-        except:
+        except Exception:
             await protocol.close()
             transport.close()
             raise
