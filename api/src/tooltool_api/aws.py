5a2c18a59d862f7cf5d7355159cfeec69ea33125
Ben Hearsum <bhearsum@mozilla.com>
2019-12-12 11:19:25 -0500

    Import and cleanup catlee's initial work for using cloudfront

--- api/src/tooltool_api/aws.py
+++ api/src/tooltool_api/aws.py
@@ -4,21 +4,15 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 import importlib
-import json
-import threading
-import time
 
 import boto
+import boto.cloudfront.distribution
 import boto.s3
 import boto.sqs
 
 import tooltool_api.lib.log
 
 logger = tooltool_api.lib.log.get_logger(__name__)
-
-
-class StopListening(Exception):
-    pass
 
 
 class AWS(object):
@@ -61,69 +55,7 @@ class AWS(object):
         # the other services
         return boto.s3.connect_to_region(region_name=region_name, aws_access_key_id=self.access_key_id, aws_secret_access_key=self.secret_access_key)
 
-    def get_sqs_queue(self, region_name, queue_name):
-        key = (region_name, queue_name)
-        if key in self._queues:
-            return self._queues[key]
-
-        sqs = self.connect_to("sqs", region_name)
-        queue = sqs.get_queue(queue_name)
-        if not queue:
-            raise RuntimeError("no such queue %r in %s" % (queue_name, region_name))
-        self._queues[key] = queue
-        return queue
-
-    def sqs_write(self, region_name, queue_name, body):
-        queue = self.get_sqs_queue(region_name, queue_name)
-        m = boto.sqs.message.Message(body=json.dumps(body))
-        queue.write(m)
-
-    def sqs_listen(self, region_name, queue_name, read_args=None):
-        def decorate(func):
-            self._listeners.append((region_name, queue_name, read_args or {}, func))
-            return func
-
-        return decorate
-
-    def _listen_thd(self, region_name, queue_name, read_args, listener):
-        logger.info("Listening to SQS queue %r in region %s", queue_name, region_name)
-        try:
-            queue = self.get_sqs_queue(region_name, queue_name)
-        except Exception:
-            logger.exception("While getting queue %r in region %s; listening cancelled", queue_name, region_name)
-            return
-
-        while True:
-            msg = queue.read(wait_time_seconds=20, **read_args)
-            if msg:
-                try:
-                    listener(msg)
-                except StopListening:  # for tests
-                    break
-                except Exception:
-                    logger.exception("while invoking %r", listener)
-                    # note that we do nothing with the message; it will
-                    # remain invisible for a while, then reappear and maybe
-                    # cause another exception
-                    continue
-                msg.delete()
-
-    def _spawn_sqs_listeners(self, _testing=False):
-        # launch a listening thread for each SQS queue
-        threads = []
-        for region_name, queue_name, read_args, listener in self._listeners:
-            thd = threading.Thread(
-                name="%s/%r -> %r" % (region_name, queue_name, listener), target=self._listen_thd, args=(region_name, queue_name, read_args, listener)
-            )
-            # set the thread to daemon so that SIGINT will kill the process
-            thd.daemon = True
-            thd.start()
-            threads.append(thd)
-
-        # sleep forever, or until we get a SIGINT, at which point the remaining
-        # threads will be killed during process shutdown
-        if not _testing:  # pragma: no cover
-            while True:
-                time.sleep(2 ** 31)
-
-        return threads
+    def connect_to_cloudfront(self, service_name, region_name):
+        # special case for cloudfront, which boto does differently than
+        # the other services
+        return boto.cloudfront.Distribution(aws_access_key_id=self.access_key_id, aws_secret_access_key=self.secret_access_key,)
