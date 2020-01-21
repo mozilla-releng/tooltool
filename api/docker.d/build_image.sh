30409706276cd28def6fd124ea300e8e80612292
Ben Hearsum <bhearsum@mozilla.com>
2020-01-21 11:00:29 -0500

    Copy frontend config into place for stage/production builds.

--- api/docker.d/build_image.sh
+++ api/docker.d/build_image.sh
@@ -4,5 +4,7 @@ set -e
 test $DOCKER_REPO
 test $DOCKER_TAG
 
+cp ./src/tooltool_api/static/${FRONTEND_CONFIG} ./src/tooltool_api/static/config.mjs
+
 docker build -f Dockerfile -t $DOCKER_REPO:$DOCKER_TAG .
 docker save $DOCKER_REPO:$DOCKER_TAG > $1
