7e7fb0771ed5964fd5c8814a308c68f7eb9ea369
Ben Hearsum <bhearsum@mozilla.com>
2020-01-22 13:09:21 -0500

    Don't try to get client credentials if we already have some

--- api/src/tooltool_api/static/tcauth.js
+++ api/src/tooltool_api/static/tcauth.js
@@ -53,43 +53,75 @@ window.tcauth.setup = function(service, default_service_url) {
     var service_url = $('body').attr('data-' + service + '-url') || default_service_url;
 
     var auth = window.localStorage.getItem('auth');
+    var tc_auth = window.localStorage.getItem('tc_auth');
 
-    try {
-        auth = JSON.parse(auth);
-    } catch(err) {
-        auth = null;
-    }
-    if (auth != null && auth.access_token) {
+    function handleLoggedIn(data) {
+        $email.html('<span>' + data.credentials.clientId + '</span>' + '<span class="caret"></span/>');
+        $login.toggleClass('hidden');
+        $loggedin.toggleClass('hidden');
         $.ajax({
-            url: config.taskclusterRootUrl + '/login/oauth/credentials',
+            url: service_url + '/init',
             async: false,
-            headers: {
-                Authorization: 'Bearer ' + auth.access_token,
-                'Content-Type': 'application/json',
-            },
-            error: function(xhr, status, error) {
-                alert("Failed to get credentials: " + error);
-                console.log("Failed to get credentials: " + error);
+            beforeSend: function (xhr, config) {
+                xhr.setRequestHeader("Authorization", tcauth.get_header(config.url, config.method));
             },
             success: function(data, status, xhr) {
-                $email.html('<span>' + data.credentials.clientId + '</span>' + '<span class="caret"></span/>');
-                $login.toggleClass('hidden');
-                $loggedin.toggleClass('hidden');
-                window.localStorage.setItem('tc_auth', JSON.stringify(data));
-                $.ajax({
-                    url: service_url + '/init',
-                    async: false,
-                    beforeSend: function (xhr, config) {
-                        xhr.setRequestHeader("Authorization", tcauth.get_header(config.url, config.method));
-                    },
-                    success: function(data, status, xhr) {
-                        angular.module('initial_data', []).constant('initial_data', data || {});
-                    }
-                });
+                angular.module('initial_data', []).constant('initial_data', data || {});
             }
         });
-    } else {
-        angular.module('initial_data', []).constant('initial_data', {});
+    }
+
+    try {
+        tc_auth = JSON.parse(tc_auth);
+    } catch(err) {
+        tc_auth = null;
+    }
+
+    // If we already have credentials, see if they're still valid
+    if (tc_auth) {
+        var expires = moment(tc_auth.expires);
+        var now = moment();
+
+        // If they are, great!
+        if (expires > now) {
+            handleLoggedIn(tc_auth);
+        // If not, force user reauth.
+        // It may be possible to do this in the background, but I haven't figured out how
+        } else {
+            window.localStorage.removeItem('auth');
+            window.localStorage.removeItem('tc_auth');
+        }
+    }
+    else {
+        // If there are no credentials, check for an access code (which is what
+        // should be present after the user is redirected back from the auth
+        // flow)
+        try {
+            auth = JSON.parse(auth);
+        } catch(err) {
+            auth = null;
+        }
+        // If we have one, get some credentials!
+        if (auth != null && auth.access_token) {
+            $.ajax({
+                url: config.taskclusterRootUrl + '/login/oauth/credentials',
+                async: false,
+                headers: {
+                    Authorization: 'Bearer ' + auth.access_token,
+                    'Content-Type': 'application/json',
+                },
+                error: function(xhr, status, error) {
+                    alert("Failed to get credentials: " + error);
+                    console.log("Failed to get credentials: " + error);
+                },
+                success: function(data, status, xhr) {
+                    window.localStorage.setItem('tc_auth', JSON.stringify(data));
+                    handleLoggedIn(data);
+                }
+            });
+        } else {
+            angular.module('initial_data', []).constant('initial_data', {});
+        }
     }
 
     $login.on('click', function(e) {
